ff60a6ad30594ccb21672134f48a66e38bf84735
Jhonec0814 <jhon.estebanct14@gmail.com>
2022-04-03 16:06:26 -0500

    se crea formulario y se utiliza firebase

--- Js/pintarCanciones.js
+++ Js/pintarCanciones.js
@@ -12,6 +12,7 @@ let contenedorCanciones = document.getElementById("contenedorCanciones")
     let tarjeta = document.createElement("div")
     tarjeta.classList.add("card")
     tarjeta.classList.add("h-100")
+    tarjeta.classList.add("bg-dark")
 
     let imagen = document.createElement("img")
     imagen.classList.add("card-img-top")
@@ -27,11 +28,13 @@ let contenedorCanciones = document.getElementById("contenedorCanciones")
     let nombre = document.createElement("h5")
     nombre.classList.add("fw-bold")
     nombre.classList.add("text-center")
+    nombre.classList.add("text-white")
     nombre.textContent = cancion.name
 
     let preview = document.createElement("audio")
     preview.classList.add("text-center")
     preview.setAttribute("controls","controls")
+    preview.classList.add("w-100")
     preview.src = cancion.preview_url
     
 
